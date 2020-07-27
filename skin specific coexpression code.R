d38a9a49a533a4fc6a95090174a67a6f8b35cc18
zhiweixiao <xzhiwei@umich.edu>
2020-07-27 19:37:44 -0400

    correlation comparison

--- skin specific coexpression code.R
+++ skin specific coexpression code.R
@@ -32,12 +32,71 @@ Adrenal_data = final_data[,colnames(final_data)%in%Adrenal_id]
 New_AdrenalData = Adrenal_data[rownames(New_Exposed_SkinData),]
 
 
-## How to get normalized expression data for each tissue after quantile normalization and inverse normalization
+## How to get normalized expression data for each tissue after quantiles normalization and inverse normalization
 tempdata_quantile <- normalize.quantiles(New_Exposed_SkinData)
 rownames(tempdata_quantile) <- rownames(New_Exposed_SkinData)
 colnames(tempdata_quantile) <- colnames(New_Exposed_SkinData)
 
 inverse_Exposed_SkinData <- t(apply(tempdata_quantile,1,function(x){ qnorm((rank(x)-(3/8))/(length(x)-2*(3/8)+1))}))
+
+## How to get spearman correlation matrix of each tissue's normalized expression data
+inverse_Exposed_SkinCor <- cor(t(inverse_Exposed_SkinData), method = "spearman")
+inverse_AdiposeCor <- cor(t(inverse_AdiposeData), method = "spearman")
+inverse_AdrenalCor <- cor(t(inverse_AdrenalData), method = "spearman")
+
+## How to do correlation comparison to find positive correlated gene pairs that're only significant in sun-exposed skin
+inverse_Exposed_SkinCor[inverse_Exposed_SkinCor < 0 ] <- 0
+inverse_Exposed_SkinCor[is.na(inverse_Exposed_SkinCor)] <- 0
+diag(inverse_Exposed_SkinCor) <- 0
+inverse_AdiposeCor[is.na(inverse_AdiposeCor)] <- 0
+
+compare1 <- (inverse_Exposed_SkinCor > inverse_AdiposeCor) & (inverse_Exposed_SkinCor > 0)
+inverse_Exposed_SkinCor <- ifelse(compare1, inverse_Exposed_SkinCor, 0)
+compare2 <- (inverse_Exposed_SkinCor > inverse_AdrenalCor) & (inverse_Exposed_SkinCor > 0)
+inverse_Exposed_SkinCor <- ifelse(compare2, inverse_Exposed_SkinCor, 0) # keep doing this for all other tissues 
+
+compare29 <- (inverse_Exposed_SkinCor > inverse_IntestineCor) & (inverse_Exposed_SkinCor > 0)
+inverse_final_compared <- ifelse(compare29, inverse_Exposed_SkinCor, 0)
+
+
+## linear regression formula
+index_Compared <- which(inverse_final_compared>0,arr.ind=T)
+index_Compared <- index_Compared[which(index_Compared[,1]>index_Compared[,2]),]
+index_Compared <- cbind(colnames(inverse_final_compared)[index_Compared[,1]],
+                        colnames(inverse_final_compared)[index_Compared[,2]])
+
+form <- list()
+for(i in 1:2042499){ 
+  form[[i]] = as.formula(paste(index_Compared[i,1], 
+                               paste(c(index_Compared[i,2],"SEX","AGE","BMI"),collapse = "+"), sep = "~"))
+  }
+
+## linear regression source data
+table2_simplified <- table2[,c("SEX","AGE","BMI")]
+table2_simplified <- table2_simplified[which(!apply(table2_simplified,1,function(x)any(is.na(x)))),]
+table2_simplified$SubjectID <- rownames(table2_simplified)
+
+inverse_Exposed_SkinData <- as.data.frame(t(readRDS("inverse_data/inverse_Exposed_SkinData.rds")))
+inverse_Exposed_SkinData$SubjectID <- sapply(rownames(inverse_Exposed_SkinData),
+                                             function(x)paste(unlist(strsplit(x,"-"))[1:2],collapse="-"))
+regression_source1 <- merge(table2_simplified,inverse_Exposed_SkinData,by="SubjectID")
+
+
+inverse_subcutaneous_data <- as.data.frame(t(readRDS("inverse_data/inverse_subcutaneous_data.rds")))
+inverse_subcutaneous_data$SubjectID <- sapply(rownames(inverse_subcutaneous_data),
+                                             function(x)paste(unlist(strsplit(x,"-"))[1:2],collapse="-"))
+regression_source2 <- merge(table2_simplified,inverse_subcutaneous_data,by="SubjectID")
+
+
+inverse_MuscleData <- as.data.frame(t(readRDS("inverse_data/inverse_MuscleData.rds")))
+inverse_MuscleData$SubjectID <- sapply(rownames(inverse_MuscleData),
+                                             function(x)paste(unlist(strsplit(x,"-"))[1:2],collapse="-"))
+regression_source3 <- merge(table2_simplified,inverse_MuscleData,by="SubjectID") # keep doing this for all tissue's largest subset
+
+
+
+
+
 
 
 
